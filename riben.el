4edf1cb1f6f51ceedeb839ce3b12c05004113195
Akira Komamura <akira.komamura@gmail.com>
2022-03-25 13:23:09 +0900

    Add a library header to riben.el

--- riben.el
+++ riben.el
@@ -1,5 +1,40 @@
 ;;; riben.el --- A dumb Japanese input method -*- lexical-binding: t -*-
 
+;; Copyright (C) 2022 by Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1") (posframe "1.1"))
+;; URL: https://github.com/akirak/emacs-dumb-japanese
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package is an input method for Japanese. It is backed by a Google API
+;; and does not require installation of native libraries or dictionaries.
+
+;; To use it, switch to "japanese-riben" input method using
+;; `toggle-input-method' or turn on `riben-mode' in a buffer you need to write
+;; Japanese.
+
+;;; Code:
+
 (require 'riben-google)
 (require 'riben-decode)
 
