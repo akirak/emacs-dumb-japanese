3ec7638873efdfe314a30cccee8d8c51323fa824
Akira Komamura <akira.komamura@gmail.com>
2024-08-11 23:42:32 +0900

    chore: Switch linting

---
+++ .rice-lock/default/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    avy = {
+      flake = false;
+      owner = "abo-abo";
+      repo = "avy";
+      type = "github";
+    };
+    emacsql = {
+      flake = false;
+      owner = "magit";
+      repo = "emacsql";
+      type = "github";
+    };
+    plz = {
+      flake = false;
+      owner = "alphapapa";
+      ref = "stable";
+      repo = "plz.el";
+      type = "github";
+    };
+    posframe = {
+      flake = false;
+      owner = "tumashu";
+      repo = "posframe";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
