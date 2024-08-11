20c71ff1c63f9fd5243dbb70765daced46c0651d
Akira Komamura <akira.komamura@gmail.com>
2024-08-11 23:44:12 +0900

    chorfe: Update copyright year

--- riben.el
+++ riben.el
@@ -1,6 +1,6 @@
 ;;; riben.el --- A dumb Japanese input method -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022 by Akira Komamura
+;; Copyright (C) 2022,2023,2024 by Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
 ;; Version: 0.1
